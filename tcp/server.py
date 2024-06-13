e7e686beb65786c1903c3a992431b9720946ce22
Prime_Saad <123338307+SaadARazzaq@users.noreply.github.com>
2024-06-13 23:07:19 +0500

    Building a TCP Server

--- tcp/server.py
+++ tcp/server.py
@@ -12,15 +12,22 @@ def main():
     server.listen(5)  #  This indicates how many connections it can support. In this case it will listen upto 5 connections
     print(f'[*] Listening on {IP}:{PORT}')
 
+    # Continuously accept new connections
     while True:
+        # Accept a new connection
+        # CLIENT is a new socket object usable to send and receive data on the connection
+        # ADDR is the address bound to the socket on the other end of the connection
         CLIENT, ADDR = server.accept()
         print(f'[*] Accepted connection from {ADDR[0]}:{ADDR[1]}')
 
+        # Create a new thread to handle the client connection
         client_handler = threading.Thread(target= handle_client, args= (CLIENT, ))
         client_handler.start()
 
 def handle_client(client_socket):
     with client_socket as sock:
+        # Receive data from the client
+        # The buffer size is 1024 bytes
         request = sock.recv(1024)
         print(f'[*] Recieved: {request.decode("utf-8")}')
 
