7dd3a29bb465748540b68c8aca3344a166c7fa24
Prime_Saad <123338307+SaadARazzaq@users.noreply.github.com>
2024-06-13 23:04:42 +0500

    Create server.py

---
+++ tcp/server.py
@@ -0,0 +1,30 @@
+import socket
+import threading  #  To accept multiple connections simultaneously
+
+IP = '0.0.0.0'  # 0.0.0.0 is to Listen on all interfaces
+PORT = 9988
+socket_tuple = (IP, PORT)
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(socket_tuple)
+    
+    server.listen(5)  #  This indicates how many connections it can support. In this case it will listen upto 5 connections
+    print(f'[*] Listening on {IP}:{PORT}')
+
+    while True:
+        CLIENT, ADDR = server.accept()
+        print(f'[*] Accepted connection from {ADDR[0]}:{ADDR[1]}')
+
+        client_handler = threading.Thread(target= handle_client, args= (CLIENT, ))
+        client_handler.start()
+
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Recieved: {request.decode("utf-8")}')
+
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
